2a1679820a6a42d8fe3aac0a9bbf9d77130c2f47
foggynight <foggynight709@gmail.com>
2021-01-02 23:20:44 -0330

    Add game loop skeleton

--- game/math/drill/src/main.cpp
+++ game/math/drill/src/main.cpp
@@ -10,6 +10,22 @@ int main()
     unsigned int count = 0;
     unsigned int difficulty = 1;
 
+    Drill drill;
+    double result;
+
+    for (int i = 0; i < count; ++i) {
+        drill.genExpr(difficulty);
+        // Output expression
+        // Get input result
+        if (result == drill.getRes()) {
+            // Output correct
+            // Increase score
+        }
+        else {
+            // Output incorrect
+        }
+    }
+
     return 0;
 }
 
