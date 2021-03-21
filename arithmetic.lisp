88655cf886aac3e122a686c1186fe554bd10dbb6
foggynight <foggynight709@gmail.com>
2021-03-21 19:13:58 -0230

    Perform drills in a loop while tracking score

--- arithmetic.lisp
+++ arithmetic.lisp
@@ -10,7 +10,12 @@
 ;; Copyright (C) 2021 Robert Coffey
 ;; All rights reserved
 
-(setf *random-state* (make-random-state t))
+(setq *random-state* (make-random-state t))
+
+(defun get-expr-count ()
+  (write-string "Expression count: ")
+  (finish-output)
+  (read))
 
 (defun get-random-op ()
   (nth (random 4) '(+ - * /)))
@@ -21,7 +26,6 @@
         (min (+ (random 10) 1) 9)))
 
 (defun print-expr-infix (expr)
-  (fresh-line)
   (format T "~d ~a ~d = "
           (cadr expr)
           (car expr)
@@ -33,9 +37,18 @@
 
 (defun main ()
   (let ((score 0)
-        (expr (gen-expr)))
-    (print-expr-infix expr)
-    (prin1 (check-result expr (read)))
-    (terpri)))
+        (expr-count (get-expr-count)))
+    (terpri)
+    (dotimes (n expr-count)
+      (let ((expr (gen-expr))
+            (result 0))
+        (print-expr-infix expr)
+        (setq result (read))
+        (if (check-result expr result)
+            (setq score (1+ score)))))
+    (terpri)
+    (format T "Final score: ~d/~d"
+            score
+            expr-count)))
 
 (main)
